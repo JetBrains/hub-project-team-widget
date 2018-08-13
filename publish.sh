66f2ce68e30ac24bfd090b52ce2571311ab062da
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-08-13 17:57:01 +0300

    Use proper environment variables [Publish]

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@
 zip -r dist.zip dist
 
 curl -X POST -L \
-   -F "password=$PLUGINS_PASSWORD" \
+   -F "password=$pluginsRepositoryLogin" \
    -F "pluginId=10467" \
-   -F "userName=$PLUGINS_USERNAME" \
+   -F "userName=$pluginsRepositoryPassword" \
    -F "file=@dist.zip;type=application/zip" \
    "https://plugins.jetbrains.com/plugin/uploadPlugin"
