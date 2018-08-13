be2a857ca420173c7ee14f5c89f053e3dc3ff30b
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-08-13 19:46:58 +0300

    Exit with non-zero code if failed to publish [Publish]

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 
 echo Publishing under $pluginsRepositoryLogin
 
-curl -X POST -L -F "userName=$pluginsRepositoryLogin" -F "pluginId=10467" -F "password=$pluginsRepositoryPassword" -F "file=@dist.zip;type=application/zip" https://plugins.jetbrains.com/plugin/uploadPlugin
+curl -X POST -L -v --fail -F "userName=$pluginsRepositoryLogin" -F "pluginId=10467" -F "password=$pluginsRepositoryPassword" -F "file=@dist.zip;type=application/zip" https://plugins.jetbrains.com/plugin/uploadPlugin
