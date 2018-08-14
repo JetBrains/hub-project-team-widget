b6ed3271050b8ecbcc6a6a8662d43d1de031b18c
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-08-14 11:15:54 +0300

    Remove publishing script to be replaced with TeamCity build step

--- publish.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-curl -X POST -L -v --fail -H "Authorization: Bearer $pluginsRepositoryToken" -F "pluginId=10467" -F "file=@dist.zip;type=application/zip" https://plugins.jetbrains.com/plugin/uploadPlugin
