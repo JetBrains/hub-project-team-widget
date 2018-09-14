c75aec2a14fe5075959fe1423307e6568393989b
katriyna <katriyna@gmail.com>
2018-09-14 14:03:54 +0300

    map translations keys after po-file are imported

--- src/app/translations.js
+++ src/app/translations.js
@@ -1,7 +1,13 @@
-import {de} from './translations/hub-project-team-widget_de.po';
-import {es} from './translations/hub-project-team-widget_es.po';
-import {fr} from './translations/hub-project-team-widget_fr.po';
-import {ru} from './translations/hub-project-team-widget_ru.po';
-import {ja} from './translations/hub-project-team-widget_ja.po';
+import de from './translations/hub-project-team-widget_de.po';
+import es from './translations/hub-project-team-widget_es.po';
+import fr from './translations/hub-project-team-widget_fr.po';
+import ru from './translations/hub-project-team-widget_ru.po';
+import ja from './translations/hub-project-team-widget_ja.po';
 
-export default {de, es, fr, ru, ja};
+export default {
+  de: de.de_DE,
+  es: es.es_ES,
+  fr: fr.fr_FR,
+  ru: ru.ru_RU,
+  ja: ja.ja_JP
+};
