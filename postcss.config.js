6090ebbdf4e67b07968a76611c7b7a27149037ed
Andrey <andrey.skladchikov@gmail.com>
2019-01-09 22:23:46 +0300

    Move to CSS variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,16 @@
-module.exports = () => ({
+module.exports = ctx => ({
   plugins: [
+    require('postcss-import'),
     require('postcss-modules-values-replace')({}),
     require('postcss-cssnext')({
       features: {
         calc: {
           mediaQueries: true
         },
-        customProperties: false
+        customProperties: {
+          preserve: true,
+          variables: ctx.options.variables
+        }
       }
     })
   ]
